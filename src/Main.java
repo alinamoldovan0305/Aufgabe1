e2a88f2004de9a0bf6b4b6b24cdb390469b8c828
Moldovan Alina <amoldovan157@gmail.com>
2025-10-19 18:25:56 +0300

    3-ter Subpunkt und main

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,12 @@ public class Main {
         //1-ter Subpunkt
         int[] ergebnis=aufgabe1.nichtausreichendeNoten(noten);
         System.out.println("Nicht ausreichende Noten: "+ Arrays.toString(ergebnis));
+
+        //2-ter Subpunkt
+        System.out.println("Durchschnittert: "+ aufgabe1.durchschnittwert(noten));
+
+        //3-ter Subpunkt
+        System.out.println("Abgerundete Noten: "+Arrays.toString(aufgabe1.abgerundetenNoten(noten)));
+
     }
 }
